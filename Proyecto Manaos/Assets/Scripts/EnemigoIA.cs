47d18f522949af0bb3f6385bf432f56bb640dc65
Marcoswaffo <89618958+Marcoswaffo@users.noreply.github.com>
2021-11-11 10:41:03 -0300

    IA de enemigo

---
+++ Proyecto Manaos/Assets/Scripts/EnemigoIA.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoIA : MonoBehaviour
+{
+    public float speed;
+    public float checkRadio;
+    public float attackRadio;
+
+    public bool shouldRotate;
+
+    public LayerMask Player;
+
+    private Transform objetivo;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Vector2 mov;
+    public Vector3 dir;
+
+    private bool enRango;
+    private bool enRangoAtaque;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        objetivo = GameObject.FindWithTag("Player").transform;
+    }
+    private void Update()
+    {
+        anim.SetBool("Corriendo", enRango);
+
+        enRango = Physics2D.OverlapCircle(transform.position, checkRadio, Player);
+        enRangoAtaque = Physics2D.OverlapCircle(transform.position, attackRadio, Player);
+
+        dir = objetivo.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        dir.Normalize();
+        mov = dir;
+        if (shouldRotate)
+        {
+            anim.SetFloat("x", dir.x);
+            anim.SetFloat("y", dir.y);
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (enRango && !enRangoAtaque)
+        {
+            MoveCharacter(mov);
+        }
+        if (enRangoAtaque)
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void MoveCharacter(Vector2 dir)
+    {
+        rb.MovePosition((Vector2)transform.position + (dir * speed * Time.deltaTime));
+    }
+}
